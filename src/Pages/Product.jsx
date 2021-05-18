7676ebb26978a5d9a84cc8aa9cbe365e964c4145
Florin Avram <avram.florin@gmail.com>
2021-05-18 20:40:29 +0300

    nefinalizat

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import './Product.css';
-import products from '../utils/products.json';
+// import products from '../utils/products.json';
 
 class Product extends React.Component {
 
   componentDidMount() {
     //console.log(products, Object.values(products));
+    /*
     Object.values(products).forEach(value=>{
       value.items.find(row => {
         row.id === this.props.match.params.productId;
       });
       //console.log(value);
     });
-
+    */
 
   }
 
